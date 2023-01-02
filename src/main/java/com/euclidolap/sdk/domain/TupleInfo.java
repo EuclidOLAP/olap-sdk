6bc4eae66a1e21fdcf1a4e402b6179ac83fb541c
ChenZhigang <chenzg@tiens.com>
2023-01-02 15:57:09 +0800

    update

--- src/main/java/com/euclidolap/sdk/domain/TupleInfo.java
+++ src/main/java/com/euclidolap/sdk/domain/TupleInfo.java
@@ -2,13 +2,16 @@ package com.euclidolap.sdk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TupleInfo {
 
     private List<MemberInfo> ms = new ArrayList<>();
+    private String display;
 
     public void add(MemberInfo mi) {
         ms.add(mi);
+        display = String.join(", ", ms.stream().map(MemberInfo::getDisplay).collect(Collectors.toList()));
     }
 
     public int length() {
@@ -18,4 +21,5 @@ public class TupleInfo {
     public MemberInfo getMemberInfo(int i) {
         return ms.get(i);
     }
+
 }
